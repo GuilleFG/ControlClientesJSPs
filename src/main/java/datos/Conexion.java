0ac223514f32508ba2b31a35cc07a6c320f583a6
Guillermo Fernández <guillefergom@gmail.com>
2024-03-11 12:46:03 +0100

    cliente y ClienteDaoJDBC creado

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package datos;
 
-/**
- *
- * @author Guillermo
- */
+import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class Conexion {
     
+    private static final String JDBC_URL= "jdbc:mysql://localhost:3306/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&aallowPublicKeyRetrieval=true";
+    
+    private static final String JDBC_USER = "root";
+    
+    private static final String JDBC_PASSWORD = "1234";
+    
+    
+    public static DataSource getDatasource(){
+            BasicDataSource ds = new BasicDataSource();
+            ds.setUrl(JDBC_URL);
+            ds.setUsername(JDBC_USER);
+            ds.setPassword(JDBC_PASSWORD);
+            ds.setInitialSize(50);
+            
+            return ds;
+    }
+    
+    public static Connection getConnection() throws SQLException{
+        return getDatasource().getConnection();
+    }
+    
+    
+    public static void close (ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        
+    }
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
 }
